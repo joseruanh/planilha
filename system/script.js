e2b3dfb7def39a69b73d992cecdb15acad751e9c
joseruanh <joseruanh2@gmail.com>
2022-01-21 17:57:25 -0200

    subindo projeto

---
+++ system/script.js
@@ -0,0 +1,43 @@
+function jogos () {
+
+    let content = window.document.getElementById("res");
+
+    content.innerHTML = `
+    
+    <img id="cblogo" src="images/img01.png" alt="CBLOL">
+            <br>
+            <br>
+    <p><strong>Rodada 1</strong></p>
+    <br>
+    <pre>
+    LLL x FUR
+    NET x FLA
+    KBM x ITZ
+    PNG x LIB
+    RNG x RED
+    </pre>
+    
+    `
+}
+
+function pontos () {
+    let content = window.document.getElementById("res");
+
+    content.innerHTML = `
+    
+    <img id="cblogo" src="images/img01.png" alt="CBLOL">
+            <br>
+            <p>Testando Pontos</p>
+    `
+}
+
+function classificacao () {
+    let content = window.document.getElementById("res");
+
+    content.innerHTML = `
+    
+    <img id="cblogo" src="images/img01.png" alt="CBLOL">
+            <br>
+            <p>Testando Classificação</p>
+    `
+}
