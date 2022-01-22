1a2f81c556878309b8f03362fe72074a0e4816de
joseruanh <joseruanh2@gmail.com>
2022-01-22 00:20:53 -0200

    adicionando votos

--- system/script.js
+++ system/script.js
@@ -79,7 +79,7 @@ function pontos () {
 			</tr>
 
 			<tr>
-				<td> <strong> Brener </strong> </td><td>jogo1</td><td>jogo2</td><td>jogo3</td><td>jogo4</td><td>jogo5</td><td> <strong> 0 pontos </strong> </td>
+				<td> <strong> Brener </strong> </td><td>LLL</td><td>FLA</td><td>KBM</td><td>PNG</td><td>RED</td><td> <strong> 0 pontos </strong> </td>
 			</tr>
 		</table>
     `
