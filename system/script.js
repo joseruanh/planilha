964423601fdabe9daa3becd5902073acb041ac9e
joseruanh <joseruanh2@gmail.com>
2022-01-22 00:05:32 -0200

    adicionando tabela

--- system/script.js
+++ system/script.js
@@ -93,6 +93,53 @@ function classificacao () {
     
     <img id="cblogo" src="images/img01.png" alt="CBLOL">
             <br>
-            <p>Testando Classificação</p>
+            <p>Classificação CBLOL</p>
+            <br>
+
+            	<table class="classificacao">
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/fla.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/png.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/lll.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/rng.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/itz.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/net.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/lib.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/fur.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/red.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	<tr>
+            		<td> <h1>1</h1> </td> <td> <img class="imgLogo1" src="logos/net.png"> </td> <td>0v - 0d</td>
+            	</tr>
+
+            	</table>
+
+		</div>
     `
 }
