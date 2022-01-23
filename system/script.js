1a77221d56b01653540533b04763047edabd7872
joseruanh <joseruanh2@gmail.com>
2022-01-22 23:24:51 -0200

    enviando voto

--- system/script.js
+++ system/script.js
@@ -91,7 +91,7 @@ function pontos () {
             </tr>
 
             <tr>
-                <td> <strong> Ruanh </strong> </td><td>jogo1</td><td>jogo2</td><td>jogo3</td><td>jogo4</td><td>jogo5</td><td> <strong> 0 pontos </strong> </td>
+                <td> <strong> Ruanh </strong> </td><td>PNG</td><td>LIB</td><td>RED</td><td>NET</td><td>FUR</td><td> <strong> 0 pontos </strong> </td>
             </tr>
 
             <tr>
